89488a4ae9882f42af5ca541d366d2f08f13f534
johnnysee <js.johnny.see@gmail.com>
2021-09-14 13:17:37 +0200

    Implemented some code in EmployeeModal.jsx

--- src/components/EmployeeModal.jsx
+++ src/components/EmployeeModal.jsx
@@ -1,9 +1,51 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import { Modal } from "semantic-ui-react";
 
 const EmployeeModal = () => {
   const [open, setOpen] = useState(false);
   const [singleEmployee, setSingleEmployee] = useState({});
-  return <div></div>;
+
+  const getEmployee = async () => {
+    let response = await axios.get(`https://reqres.in/api/users/${id}`);
+    setSingleEmployee;
+    response.data.data;
+  };
+  useEffect(() => {
+    getEmployee;
+  }, []);
+
+  return;
+  <Modal
+    onClose={() => setOpen(false)}
+    onOpen={() => setOpen(true)}
+    open={open}
+    trigger={
+      <Button size="tiny" positive className="view-button">
+        View
+      </Button>
+    }
+  >
+    <Modal.Content image id="modal-container">
+      <Image
+        className="image"
+        size="small"
+        src={singleEmployee.avatar}
+        wrapped
+      />
+      <Modal.Description>
+        <Header className="name">
+          {singleEmployee.first_name} {singleEmployee.last_name}
+        </Header>
+        <p className="email">Email: {singleEmployee.email}</p>
+      </Modal.Description>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button color="black">Edit</Button>
+      <Button negative>Delete</Button>
+    </Modal.Actions>
+  </Modal>;
 };
 
 export default EmployeeModal;
