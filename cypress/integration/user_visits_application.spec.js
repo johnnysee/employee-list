ab2e44f667355f3b1cebefd9c684f3c93cb9cf48
johnnysee <js.johnny.see@gmail.com>
2021-09-14 10:43:08 +0200

    User spec file fully pass now

--- cypress/integration/user_visits_application.spec.js
+++ cypress/integration/user_visits_application.spec.js
@@ -1,32 +1,27 @@
-describe('user visits the application', () => {
+describe("user visits the application", () => {
   before(() => {
-    cy.intercept('GET','*/users**', {fixture: 'users.json'})
-    cy.visit('/')
-  })
-  it('is expected to display title', () => {
-    cy.get('[data-testid=header]').should('contain.text', 'Employee Management')
-  })
-  
-  describe('Employee List Section', () => {
-    it('is expected to display 4 employees', () => {
-      cy.get('[data-testid=employee-list').children().should('have.length', 4)
-      })
-      it('is expected to return Thomas as first employee in list', () => {
-        cy.get('[data-testid=employee-list]')
-        .children()
-        .first()
-        .should('contain.text', 'Thomas Bluth')
-        .next()
-        .should("contain.text", "Janet")
-      })
+    cy.intercept("GET", "*/users**", { fixture: "users.json" });
+    cy.visit("/");
+  });
+  it("is expected to display title", () => {
+    cy.get("#header").should("contain.text", "Employee list");
+  });
 
-      it('is expected list items to display an image', () => {
-        cy.get('#employee-list').within(() => {
-          cy.get('.employee-item')
-          .first()
-          .find('.avatar')
-          .should('be.visible')
-        })
-      })
-    })
-  })
+  it("is expected to display 4 employees", () => {
+    cy.get("#employee-list").children().should("have.length", 4);
+  });
+
+  it("is expected that the list items display the expected content", () => {
+    cy.get("#employee-list")
+      .children()
+      .first()
+      .find(".name")
+      .should("contain", "Thomas Bluth");
+  });
+
+  it("is expected list items to display an image", () => {
+    cy.get("#employee-list").within(() => {
+      cy.get(".employee-item").first().find(".avatar").should("be.visible");
+    });
+  });
+});
